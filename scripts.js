4484c78c1277ce7601c4e846a91b5b734577e62a
Mateo <matthew27garcia@gmail.com>
2022-01-20 23:25:56 -0600

    added the const of the data within the html

---
+++ scripts.js
@@ -0,0 +1,17 @@
+class Calculator {
+    constructer(previousOperandTextElement, currentOperandTextElement) {
+      this.previousOperandTextElement = previousOperandTextElement;  
+      this.currentOperandTextElement = currentOperandTextElement;
+    } 
+}
+
+
+
+
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+const CurrentTextElement = document.querySelector('[data-current-operand]')
