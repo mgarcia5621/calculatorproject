ea1ae3629c2abd6406b31f28e2a6766471a89f96
Mateo <matthew27garcia@gmail.com>
2022-01-22 15:26:56 -0600

    added the lines of js

--- scripts.js
+++ scripts.js
@@ -2,7 +2,34 @@ class Calculator {
     constructer(previousOperandTextElement, currentOperandTextElement) {
       this.previousOperandTextElement = previousOperandTextElement;  
       this.currentOperandTextElement = currentOperandTextElement;
+      this.clear()
     } 
+
+    clear() {
+      this.currentOperand = ''
+      this.previousOperand = ''
+      this.operation = undefined
+    }
+
+    delete() {
+
+    }
+
+    appendNumber(number) {
+
+    }
+
+    chooseOperations(operations) {
+
+    }
+
+    compute() {
+
+    }
+
+    updateDisplay() {
+      
+    }
 }
 
 
@@ -15,3 +42,11 @@ const deleteButton = document.querySelector('[data-delete]')
 const allClearButton = document.querySelector('[data-all-clear]')
 const previousOperandTextElement = document.querySelector('[data-previous-operand]')
 const CurrentTextElement = document.querySelector('[data-current-operand]')
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+
+numberButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    calculator.appendNumber(button.innerText)
+  })
+})
