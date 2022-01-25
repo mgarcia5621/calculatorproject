87bac5dfceec012de9d24ecb92192673965442a1
Mateo <matthew27garcia@gmail.com>
2022-01-25 00:28:44 -0600

    added few lines but still need help

--- scripts.js
+++ scripts.js
@@ -16,7 +16,7 @@ class Calculator {
     }
 
     appendNumber(number) {
-
+      this.currentOperand = this.currentOperand.toString() + number.toString()
     }
 
     chooseOperations(operations) {
