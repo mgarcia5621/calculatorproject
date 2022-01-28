a3e1e3a5e283f44649f9dce6b8f98aadbde6270d
Mateo <matthew27garcia@gmail.com>
2022-01-28 00:05:42 -0600

    added the clear button and the functions along with it

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ class Calculator {
     }
 
     delete() {
-
+      this.currentOperand = this.currentOperand.toString().slice(0, -1)
     }
 
     appendNumber(number) {
@@ -32,7 +32,29 @@ class Calculator {
 
     compute() {
       let computation 
-      const prev = parseFloat()
+      const prev = parseFloat(this.previousOperand)
+      const current = parseFloat(this.currentOperand)
+      if (isNaN(prev) || isNaN(current)) return
+      switch (this.operation) {
+        case '+':
+          computation = prev + current
+          break
+        case '-':
+            computation = prev - current
+         break
+        case '*':
+              computation = prev * current
+         break
+        case '÷':
+             computation = prev / current
+         break
+         default:
+           return
+                      
+      }
+      this.currentOperand = computation
+      this.operation = undefined
+      this.previous = ''
     }
 
     updateDisplay() {
@@ -73,3 +95,7 @@ equalsButton.addEventListener('click', button => {
   calculator.updateDisplay()
 })
 
+allClearButton.addEventListener('click', button => {
+  calculator.clear()
+  calculator.updateDisplay()
+})
