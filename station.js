07fb2a4e5665bc48a01c531360fe590da609f609
Mateo <matthew27garcia@gmail.com>
2022-01-28 22:36:10 -0600

    added another file

---
+++ station.js
@@ -0,0 +1,28 @@
+//document.getElementById('count-el').innerText = 5
+
+//initialize the count as 0 
+//listen for clicks on the increment button
+// increment the count variable when the button is clicked
+// change the count-el in the HTML to reflect the new count
+
+
+// this is to create a function or to declare such one
+function myLogger() {
+    console.log(42)
+}
+
+myLogger()
+
+let lap1 = 34 
+let lap2 = 33
+let lap3 = 36
+
+
+function logLapTime() {
+    let totalTime = lap1 + lap2 + lap3
+    console.log(totalTime)
+}
+
+console.log(totalTime)
+
+logLapTime()
