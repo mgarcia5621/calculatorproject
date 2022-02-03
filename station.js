98b35b7d3bb3f46b2512e8f16bf0ca1db661b106
Mateo <matthew27garcia@gmail.com>
2022-02-02 19:22:57 -0600

    added few lines of js

--- station.js
+++ station.js
@@ -9,10 +9,10 @@
 // this is to create a function or to declare such one
 
 
+"use strict";
+
 let countEl = document.getElementById('count-el')
 
-
-console.log(countEl)
 
 let count = 0 
 
@@ -22,3 +22,27 @@ function increment () {
 }
 
 increment()
+
+
+function save () {
+    console.log(count)   
+}
+
+save()
+
+var price = 20;
+var isSale = true;
+
+//variable shadowing
+//let & const - block-scoped
+
+//any var created within a block doesnt exist and cannot be accessed outside of the block
+
+if (isSale) {
+  // discount price of product
+  let price = 20 - 2; 
+  // do something 
+  console.log('sale price', price);
+}
+
+console.log('price', price);
